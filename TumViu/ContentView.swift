87833e582fe7557df652fc6cc3d0219a32169a9a
ThumViu <appdevelopp2121@gmail.com>
2021-10-08 22:07:07 -0600

    first commit

---
+++ TumViu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TumViu
+//
+//  Created by Abraham De Leon on 10/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TumViu/TumViuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TumViuApp.swift
+//  TumViu
+//
+//  Created by Abraham De Leon on 10/8/21.
+//
+
+import SwiftUI
+
+@main
+struct TumViuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
